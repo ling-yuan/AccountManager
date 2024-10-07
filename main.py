2fc45b4a598543be353219a934706517aa2ab308
白石 <572268633@qq.com>
2024-10-07 20:10:31 +0800

    fix: 设置启动参数

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setQuitOnLastWindowClosed(False)
     myWin = MyMainWindow(app=app)
-    myWin.show()
+
+    # 启动参数中如果不指定是否打开窗口，则默认打开
+    if "--no-window" not in sys.argv:
+        myWin.show()
+
     sys.exit(app.exec_())
